1426965a0801eae15428767172adbaeb851578d6
Techamek <70824883+Techamek@users.noreply.github.com>
2025-10-26 14:03:04 -0400

    Add CRUD operations for instructor, student, and section

---
+++ CRUD-examples.sql
@@ -0,0 +1,26 @@
+-- Instructor CRUD
+INSERT INTO instructor (...);
+SELECT ... FROM instructor;
+UPDATE instructor ...;
+DELETE FROM instructor ...;
+
+-- Student CRUD
+INSERT INTO student (...);
+SELECT ... FROM student;
+UPDATE student ...;
+DELETE FROM student ...;
+
+-- Section CRUD
+INSERT INTO section (...);
+SELECT ...;
+UPDATE ...;
+DELETE ...;
+
+-- Enroll a student
+INSERT INTO enrollment (...);
+-- Assign instructor
+INSERT INTO teaches (...);
+-- Drop class
+DELETE FROM enrollment ...;
+-- Give grade
+UPDATE enrollment ...;
