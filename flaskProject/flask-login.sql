d8764743fc1a712e3fd1aab1a781d0a4c7194db8
Ben <127755097+Benams5211@users.noreply.github.com>
2025-11-26 18:04:52 -0500

    Update flask-login.sql

--- flaskProject/flask-login.sql
+++ flaskProject/flask-login.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
         `username` varchar(50) NOT NULL,
         `password` varchar(255) NOT NULL,
         `email` varchar(100) NOT NULL,
-        `role` varchar(1) NOT NULL,
+        `role` varchar(15) NOT NULL,
     PRIMARY KEY (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
